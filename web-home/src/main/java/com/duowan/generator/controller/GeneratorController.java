2ec964da80e3de613c1ba6a915bc97acceed20ef
badqiu <badqiu@gmail.com>
2015-10-10 18:30:37 +0800

    1

--- web-home/src/main/java/com/duowan/generator/controller/GeneratorController.java
+++ web-home/src/main/java/com/duowan/generator/controller/GeneratorController.java
@@ -178,7 +178,7 @@ public class GeneratorController {
 			AntPathMatcher pathMatcher = new AntPathMatcher();
 			Collection<File> files = FileUtils.listFiles(new File(outRoot), null, true);
 			for(File f : files) {
-				System.out.println("copy2MutiProjectDirLayout,file:"+f);
+				System.out.println("copy2MutiProjectDirLayout,file:"+f.getPath());
 				for(String[] mapping : mappings) {
 					String pattern = mapping[0];
 					String targetDir = mapping[1];
