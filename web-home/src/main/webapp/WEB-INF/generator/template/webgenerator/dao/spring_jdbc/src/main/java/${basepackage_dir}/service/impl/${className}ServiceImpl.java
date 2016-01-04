6b90e69bc4bfe4e238460c227eaf5c0c971a7c60
badqiu <badqiu@gmail.com>
2016-01-04 16:49:12 +0800

    1

--- web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/main/java/${basepackage_dir}/service/impl/${className}ServiceImpl.java
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/main/java/${basepackage_dir}/service/impl/${className}ServiceImpl.java
@@ -61,6 +61,9 @@ public class ${className}ServiceImpl implements ${className}Service {
         return ${classNameLower};
     }	
     
+    /**
+     *  join取回${className}的关联信息,如一对多，多对一等的关联信息
+     */
     public ${className} join(${className} ${classNameLower}) {
     	return ${classNameLower};
     }
