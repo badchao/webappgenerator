2336c4a86acf932b923271628f499879a3307afb
badqiu <badqiu@gmail.com>
2019-05-10 15:15:38 +0800

    1

--- web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/test/java/${basepackage_dir}/dao/impl/${className}DaoImplTest.java
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/test/java/${basepackage_dir}/dao/impl/${className}DaoImplTest.java
@@ -50,13 +50,6 @@ public class ${className}DaoImplTest {
 		this.dao = dao;
 	}
 
-	protected String[] getDbUnitDataFiles() {
-	    //通过testName.getMethodName() 可以得到当前正在运行的测试方法名称
-//		return new String[]{"classpath:testdata/common.xml","classpath:testdata/${className}.xml",
-//		                    "classpath:testdata/${className}_"+testName.getMethodName()+".xml"};
-		return null;
-	}
-	
 	//数据库单元测试前会开始事务，结束时会回滚事务，所以测试方法可以不用关心测试数据的删除
 	@Test
 	public void findPage() {
