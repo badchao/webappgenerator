3d56358e4abe9e130878f08eaf0d626bd177aff6
badqiu <badqiu@gmail.com>
2021-02-24 20:10:16 +0800

    1

--- web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/ui_frontend/react_antd_dva_admin/${className}/index.js
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/ui_frontend/react_antd_dva_admin/${className}/index.js
@@ -34,6 +34,22 @@ class ${className}Index extends Component {
     componentWillUnmount() {
     }
     
+   /**
+         * 页面相关事件处理函数--监听用户下拉动作,当前没有实现
+     */
+    onPullDownRefresh() {
+      //this.execSearch(1);
+    }
+  
+    /**
+         * 页面上拉触底事件的处理函数,当前没有实现
+     */
+    onReachBottom() {
+      //if (this.data.hasMoreData) {
+      //  this.execSearch(this.data.page + 1);
+      //}
+    }
+  
     render() {
       //URL 参数
       const query = this.props.location.query;
