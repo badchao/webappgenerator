9ce2f9af10cec3bff613456d6d11ae651b6fb649
badqiu <badqiu@gmail.com>
2021-05-20 17:57:04 +0800

    1

--- web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/test/java/${basepackage_dir}/datafactory/${className}DataFactory.java
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/test/java/${basepackage_dir}/datafactory/${className}DataFactory.java
@@ -1,7 +1,7 @@
 <#include "/java_copyright.include">
 <#assign className = table.className>   
 <#assign classNameLower = className?uncap_first> 
-package ${basepackage};
+package ${basepackage}.datafactory;
 
 import java.util.*;
 
