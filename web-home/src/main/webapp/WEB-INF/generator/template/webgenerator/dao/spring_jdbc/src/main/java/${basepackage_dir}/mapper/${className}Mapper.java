4839e672ca978170dcf6c04217932d3ab54eb526
qiubaichao <qiubaichao@xmiles.cn>
2023-11-15 09:22:39 +0800

    1

--- web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/main/java/${basepackage_dir}/mapper/${className}Mapper.java
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/main/java/${basepackage_dir}/mapper/${className}Mapper.java
@@ -1,3 +1,6 @@
+<#include "/java_copyright.include">
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first>  
 package ${basepackage}.dao;
 
 import org.apache.ibatis.annotations.Mapper;
