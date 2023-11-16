5b0a342e385487ed40bc7f2bf82f8bb48b4fb6ea
qiubaichao <qiubaichao@xmiles.cn>
2023-11-16 19:59:40 +0800

    1

---
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/src/test/java/${basepackage_dir}/service/mybatis_impl/${className}ServiceImplTest.java
@@ -0,0 +1,84 @@
+<#include "/java_copyright.include">
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first> 
+<#assign pkJavaType = table.idColumn.javaType>   
+<#assign pkJavaVarName = table.pkColumn.columnNameFirstLower>
+
+package ${basepackage}.service.impl;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import java.util.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import ${basepackage}.datafactory.${className}DataFactory;
+import ${basepackage}.model.*;
+import ${basepackage}.query.*;
+import ${basepackage}.mapper.*;
+
+/**
+<#include "/java_description.include">
+ */
+public class ${className}ServiceImplTest {
+
+	//mock框架使用Mockito 具体使用请查看: http://code.google.com/p/mockito/wiki/MockitoVSEasyMock
+	
+	private ${className}ServiceImpl service = new ${className}ServiceImpl();
+	private ${className}Mapper ${classNameLower}Mapper = mock(${className}Mapper.class);
+	
+	@Rule public TestName testName = new TestName();
+	
+	private ${className} id = new ${className}(<@generateArgumentsWithRandomValue table.pkColumns/>);
+	
+	@Before
+	public void before() {
+		System.out.println("\n------------------ "+testName.getMethodName()+" ----------------------\n");
+		service.set${className}Mapper(${classNameLower}Mapper);
+	}
+	
+	@Test
+	public void create() {
+		${className} obj = ${className}DataFactory.new${className}();
+		service.create(obj);
+		
+		verify(${classNameLower}Mapper).insert(obj); //验证执行了该语句
+	}
+	
+	@Test
+	public void update() {
+		when(${classNameLower}Mapper.getById(id)).thenReturn(${className}DataFactory.new${className}()); // mock方法调用
+		
+		${className} obj = ${className}DataFactory.new${className}();
+		service.update(obj);
+		
+		verify(${classNameLower}Mapper).updateById(any()); //验证执行了该语句
+	}
+	
+	@Test
+	public void removeById() {
+		service.removeById(id);
+		
+		verify(${classNameLower}Mapper).deleteById(id); //验证执行了该语句
+	}
+	
+	@Test
+	public void getById() {
+		when(${classNameLower}Mapper.getById(id)).thenReturn(${className}DataFactory.new${className}()); // mock方法调用
+		
+		${className} ${classNameLower} = service.getById(id);
+		
+		verify(${classNameLower}Mapper).selectById(id); //验证执行了该语句
+		assertNotNull(${classNameLower});
+	}
+	
+	@Test
+	public void check() {
+		${className} obj = ${className}DataFactory.new${className}();
+		service.check(obj);
+	}
+}
+
