ab7b6512e997159474259800a780e413f7565db5
baichao.qiu <baichao.qiu@galaxis.com.cn>
2025-05-26 13:58:54 +0800

    1

---
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/backend_languages/rust/dto/${underscoreName}_dto_util.rs
@@ -0,0 +1,32 @@
+
+<#include "/java_copyright.include">
+<#include "/macro.include"/>
+<#assign className = table.className>   
+<#assign underscoreName = table.underscoreName>   
+
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use crate::generated::protobuf::generated_protobuf::${ClassName}Dto;
+use crate::model::ods_env_check::${ClassName};
+
+struct ${ClassName}DtoUtil;
+
+impl ${ClassName}DtoUtil{
+    pub fn from_dto(input: &${ClassName}Dto) -> ${ClassName} {
+        let result = ${ClassName} {
+			<#list table.columns as column>
+            ${column.underscoreName}: input.${column.underscoreName},
+            </#list>
+        };
+        return result;
+    }
+
+	pub fn to_dto(input: &${ClassName}) -> ${ClassName}Dto {
+        let mut result = ${ClassName}Dto{
+			<#list table.columns as column>
+            ${column.underscoreName}: input.${column.underscoreName},
+            </#list>
+		};
+		return result;
+    }
+}
