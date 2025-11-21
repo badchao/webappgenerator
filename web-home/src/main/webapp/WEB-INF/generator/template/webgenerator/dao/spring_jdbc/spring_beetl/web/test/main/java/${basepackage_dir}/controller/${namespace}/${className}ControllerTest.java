af073597b2b5f0022189bfb5912f90b398484491
qiubaichao <qiubaichao@lizhi.fm>
2025-11-21 17:22:24 +0800

    fixed test

---
+++ web-home/src/main/webapp/WEB-INF/generator/template/webgenerator/dao/spring_jdbc/spring_beetl/web/test/main/java/${basepackage_dir}/controller/${namespace}/${className}ControllerTest.java
@@ -0,0 +1,106 @@
+<#include "/java_copyright.include">
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first> 
+<#assign pkJavaType = table.idColumn.javaType>   
+<#assign pkJavaVarName = table.pkColumn.columnNameFirstLower>
+
+package ${basepackage}.controller.${namespace};
+
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import ${basepackage}.model.*;
+import ${basepackage}.query.*;
+import ${basepackage}.service.*;
+
+import com.github.rapid.common.util.page.Page;
+
+public class ${className}ControllerTest extends BaseControllerTestCase {
+
+	${className}Controller controller = new ${className}Controller();
+	
+	private ${className}Service ${classNameLower}Service = mock(${className}Service.class);
+	
+	@Rule public TestName testName = new TestName();
+	
+	private ${table.pkColumn.javaType} id = new${className}().id();
+	
+	@Before
+	public void before() {
+		System.out.println("\n------------------ "+testName.getMethodName()+" ----------------------\n");
+		controller.${classNameLower}Service = ${classNameLower}Service;
+	}
+	
+	@Test
+	public void create() {
+		${className} obj = new${className}();
+		controller.create(obj);
+		
+		verify(${classNameLower}Service).create(obj); //验证执行了该语句
+	}
+	
+
+	@Test
+	public void update() {
+		${className} obj = new${className}();
+		controller.update(obj);
+		
+		verify(${classNameLower}Service).updateById(any()); //验证执行了该语句
+	}
+	
+	@Test
+	public void remove() {
+		controller.remove(id);
+		
+		verify(${classNameLower}Service).deleteById(id); //验证执行了该语句
+	}
+	
+	@Test
+	public void getone() {
+		when(${classNameLower}Service.getone(id)).thenReturn(new${className}()); // mock方法调用
+		
+		${className} ${classNameLower} = controller.getone(id);
+		
+		verify(${classNameLower}Service).single(id); //验证执行了该语句
+		assertNotNull(${classNameLower});
+	}
+	
+	
+	@Test
+	public void query() {
+		${className}Query query = new${className}Query();
+		when(${classNameLower}Service.query(query)).thenReturn(new PageQuery<${className}>); // mock方法调用
+		
+		controller.query(query);
+		
+		verify(${classNameLower}Service).query(query); //验证执行了该语句
+	}
+
+	@Test
+	public void download() {
+		${className}Query query = new${className}Query();
+		when(${classNameLower}Service.query(query)).thenReturn(new PageQuery<${className}>); // mock方法调用
+		
+		controller.download(query);
+		
+		verify(${classNameLower}Service).query(query); //验证执行了该语句
+	}
+	
+	private ${className}Query new${className}Query() {
+		return new ${className}Query();
+	}
+	
+	private ${className} new${className}() {
+		return new ${className}();
+	}
+	
+}
